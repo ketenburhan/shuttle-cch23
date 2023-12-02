6f5f4aee6384d6c54626f1eb3c7b3991d8213ca5
Burhan Keten <ketenburhan@protonmail.com>
2023-12-02 13:26:29 +0300

    day-1 error

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, response::{Response, IntoResponse}, http::StatusCode};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn error() -> Response {
+    (StatusCode::INTERNAL_SERVER_ERROR).into_response()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(error));
 
     Ok(router.into())
 }
